0b2ad035c484da36e963e0ed3f481ce992384c46
sayangdeep <sayang.biswas@gmail.com>
2023-04-04 21:43:22 +0530

    Initial commit
    dekh lo bhai. Isse thora idea mil jayega tumko

---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputCollection = new List<Custom>();
+            var result = new List<Output>();
+            inputCollection.Add(new Custom
+            {
+                id = 1,
+                name = "coll1",
+                dates = new List<MyDates>
+                {
+                    new MyDates
+                    {
+                        name = "Older than 1 month",
+                        date = DateTime.Parse("3/1/2023 3:31:55 PM")
+                    },
+                    new MyDates
+                    {
+                        name = "Recent and should be ignored.",
+                        date = DateTime.Parse("1/4/2023 3:31:55 PM")
+                    },
+                    new MyDates
+                    {
+                        name = "Older than one month",
+                        date = DateTime.Parse("24/1/2023 3:31:55 PM")
+                    }
+                }
+            });
+            inputCollection.Add(new Custom
+            {
+                id = 2,
+                name = "coll2",
+                dates = new List<MyDates>
+                {
+                    new MyDates
+                    {
+                        name = "Recent but not latest",
+                        date = DateTime.Parse("27/3/2023 3:31:55 PM")
+                    },
+                    new MyDates
+                    {
+                        name = "Older than one month.",
+                        date = DateTime.Parse("16/2/2023 3:31:55 PM")
+                    },
+                    new MyDates
+                    {
+                        name = "Recent as well as latest. Should be ignored",
+                        date = DateTime.Parse("2/4/2023 3:31:55 PM")
+                    }
+                }
+            });
+
+            foreach (Custom item in inputCollection)
+            {
+                // Ordering the dates collection by date so that we can identify the latest date
+                var datesColl = item.dates.OrderByDescending(x => x.date).ToList();
+
+                for (int i = 0; i < datesColl.Count(); i++)
+                {
+                    if (i == 0)
+                    {
+                        // Checking if the latest date is within 30 days. If yes then ignore it.
+                        // For all other case add it.
+                        if (datesColl[i].date >= DateTime.Now.AddDays(-30))
+                        {
+                            continue;
+                        }
+                        result.Add(new Output
+                        {
+                            id = item.id,
+                            name = item.name,
+                            internalName = datesColl[i].name,
+                            date = datesColl[i].date
+                        });
+                    }
+                    else
+                    {
+                        result.Add(new Output
+                        {
+                            id = item.id,
+                            name = item.name,
+                            internalName = datesColl[i].name,
+                            date = datesColl[i].date
+                        });
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+
+    public class Custom
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public List<MyDates>? dates { get; set; }
+    }
+
+    public class MyDates
+    {
+        public string? name { get; set; }
+        public DateTime? date { get; set; }
+    }
+
+    public class Output
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public string? internalName { get; set; }
+        public DateTime? date { get; set; }
+    }
+}
